9551a0307b83d1f17cd5a9ea0da6f3d2a8e156ea
venkatesh-prasad-07 <76904939+venkatesh-prasad-07@users.noreply.github.com>
2021-03-10 15:06:16 +0530

    Create day7.py

---
+++ day7.py
@@ -0,0 +1,31 @@
+#Output Format
+
+#Print the elements of array A in reverse order as a single line of space-separated numbers.
+
+#Sample Input
+
+#4
+#1 4 3 2
+#Sample Output
+
+#2 3 4 1
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+    i=n-1
+    while i>=0:
+       print( arr[i],join())
+        i = i-1
+            
